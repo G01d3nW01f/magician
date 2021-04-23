c2f5ba3c00bdf4b34861a6214fa2a2ffdd7e3bba
Ki11i0n4ir3 <75846902+Ki11i0n4ir3@users.noreply.github.com>
2021-04-23 11:17:46 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+
+if os.getuid() != 0:
+    print("[!]You should execute as root, say the magic word ")
+    sys.exit()
+os.system("chmod +x magician.py")
+os.system("cp magician.py /usr/local/bin/magician")
+os.system("cp magician_banner.py /usr/local/bin/")
+os.system("cp magi.py /usr/local/bin/")
+os.system("cp armoury.py /usr/local/bin/")
+
